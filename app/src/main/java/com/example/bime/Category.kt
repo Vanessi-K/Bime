3183e00c03382f0305a37554729336fb169bad0b
cc211018 <cc211018@fhstp.ac.at>
2023-01-16 14:01:28 +0100

    updated classes - created day class with function

--- app/src/main/java/com/example/bime/Category.kt
+++ app/src/main/java/com/example/bime/Category.kt
@@ -1,6 +1,5 @@
 package com.example.bime
 
-//how to use string with colour?
 class Category(val id: Int? = null, var name: String? = "", var colour: String? = "") {
     override fun toString(): String {
         return "Category_id: ${id}, Category_name: ${name}, Category_colour: ${colour}";
