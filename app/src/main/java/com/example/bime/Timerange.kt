fbfeaec9280772776466c5084587dfc6f5b4185e
cc211018 <cc211018@fhstp.ac.at>
2023-01-16 17:01:39 +0100

    new function in Database "getEntriesPerTimerange" and created class Timerange

--- app/src/main/java/com/example/bime/Timerange.kt
+++ app/src/main/java/com/example/bime/Timerange.kt
@@ -2,22 +2,38 @@ package com.example.bime
 
 import android.content.Context
 import java.sql.Date
+import java.time.LocalDate
 
-class Timerange(var startDay: Date? = null, var timerange: Int? = null, private val context: Context) {
+class Timerange(var startDay: LocalDate, var timerange: Int, private val context: Context) {
 
-    val db = DatabaseHandler(this.activity)
+    val db = DatabaseHandler(this.context)
 
-    val listOfDaysInRange get() = db.getEntriesByDay(startDay.day+ timerange)
-
-    override fun toString(): String {
-        return "Timerange_startDay: ${startDay}, Timerange_timerange: ${timerange}, Timerange_listOfDaysInRange: ${listOfDaysInRange}";
+    val listOfEntriesInRange : MutableList<Day> = mutableListOf()
+    init {
+        for(i in 0..timerange) {
+            listOfEntriesInRange.add(Day(startDay.plusDays(i.toLong()), context))
+        }
     }
 
-    fun dayPerTimerange(){
-        var days = 0
-        for (i in listOfDaysInRange) {
-            days += 1
+    override fun toString(): String {
+        return "Timerange_startDay: ${startDay}, Timerange_timerange: ${timerange}, Timerange_listOfDaysInRange: ${listOfEntriesInRange}";
+    }
+
+    fun timePerCategory(category: Int): Double {
+        var time = 0.0
+        for (day in listOfEntriesInRange) {
+            time+=day.timePerCategory(category)
         }
-        return days
+
+        return time
+    }
+
+    fun timePerTimerange(): Double {
+        var time = 0.0
+        for (day in listOfEntriesInRange) {
+            time+=day.timePerDay()
+        }
+
+        return time
     }
 }
