c98388197ac3cb753145df07ab08090788f37927
Vanessi-K <vanessa.koeck.26@outlook.com>
2023-01-19 11:56:18 +0100

    Calculation of first day of the week

--- app/src/main/java/com/example/bime/fragments/WeeklyOverviewFragment.kt
+++ app/src/main/java/com/example/bime/fragments/WeeklyOverviewFragment.kt
@@ -7,6 +7,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.bime.DatabaseHandler
 import com.example.bime.R
+import com.example.bime.model.Timerange
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.PieChart
+import java.time.LocalDate
 
 class WeeklyOverviewFragment : Fragment() {
 
@@ -25,7 +29,15 @@ class WeeklyOverviewFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val passedDate = LocalDate.now();
+        val weekStartDay = passedDate.minusDays(passedDate.dayOfWeek.value.toLong() - 1)
+
         val db = DatabaseHandler(this.activity)
+
+        val weeklyBarChart = view.findViewById<BarChart>(R.id.barChart)
+
+        val weekTimerange = Timerange(weekStartDay,6, this.activity)
+        weekTimerange.createBarChart(weeklyBarChart)
 
     }
 
