feee4f46acba58b5d5061ec60edf208efff8fa8a
cc211018 <cc211018@fhstp.ac.at>
2023-01-20 12:43:51 +0100

    fixed calender save issue in edit fragment

--- app/src/main/java/com/example/bime/fragments/AddEntryFragment.kt
+++ app/src/main/java/com/example/bime/fragments/AddEntryFragment.kt
@@ -1,13 +1,10 @@
 package com.example.bime.fragments
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -55,7 +52,7 @@ class AddEntryFragment : Fragment() {
             }
         }
 
-        calendarCheck = CheckCalendar(view.findViewById(R.id.calendarView))
+        calendarCheck = CheckCalendar(view.findViewById(R.id.calendarView), LocalDate.now())
 
         radioButtonCheck = CheckRadioButtons(view.findViewById(R.id.radio_group), view.findViewById(R.id.radio_busy_time))
 
