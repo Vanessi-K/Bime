70541112332a121527d684f03bca858e1e477290
Vanessi-K <vanessa.koeck.26@outlook.com>
2023-01-23 09:08:16 +0100

    only return two numbers after decimal for scale

--- app/src/main/java/com/example/bime/classes/charts/HourFormatter.kt
+++ app/src/main/java/com/example/bime/classes/charts/HourFormatter.kt
@@ -3,14 +3,18 @@ package com.example.bime.classes.charts
 import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.formatter.ValueFormatter
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class HourFormatter : ValueFormatter() {
 
     override fun getPieLabel(value: Float, pieEntry: PieEntry?): String {
-        return "$value h"
+        val shortValue = BigDecimal(value.toDouble()).setScale(2, RoundingMode.HALF_EVEN)
+        return "$shortValue h"
     }
 
     override fun getAxisLabel(value: Float, axis: AxisBase?): String {
-        return "$value h"
+        val shortValue = BigDecimal(value.toDouble()).setScale(2, RoundingMode.HALF_EVEN)
+        return "$shortValue h"
     }
 }
