b9c4fc99f2d2f2c80792b3f2b3d8e5b39a46e07a
Vanessi-K <vanessa.koeck.26@outlook.com>
2023-01-23 09:14:04 +0100

    made arrows to change between weeks clickable

--- app/src/main/java/com/example/bime/fragments/WeeklyOverviewFragment.kt
+++ app/src/main/java/com/example/bime/fragments/WeeklyOverviewFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
@@ -81,13 +82,13 @@ class WeeklyOverviewFragment : Fragment() {
             navigateToAddEntry()
         }
 
-        view.findViewById<TextView>(R.id.previousWeek).setOnClickListener() {
-            navigateToSelf(passedDate.minusDays(7))
-        }
+        view.findViewById<TextView>(R.id.previousWeek).setOnClickListener() { navigateToSelf(passedDate.minusDays(7)) }
 
-        view.findViewById<TextView>(R.id.nextWeek).setOnClickListener() {
-            navigateToSelf(passedDate.plusDays(7))
-        }
+        view.findViewById<ImageView>(R.id.leftArrow).setOnClickListener() { navigateToSelf(passedDate.minusDays(7)) }
+
+        view.findViewById<TextView>(R.id.nextWeek).setOnClickListener() { navigateToSelf(passedDate.plusDays(7)) }
+
+        view.findViewById<ImageView>(R.id.rightArrow).setOnClickListener() { navigateToSelf(passedDate.plusDays(7)) }
     }
 
     private fun navigateToAddEntry() {
